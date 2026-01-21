47127fe874b1baabda45384e75abafed0816437b
Gautam007Joshi <gautamjoshi06042002@gmail.com>
2026-01-21 17:01:43 +0530

    push

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { db } from "../../firebase";
-import { logFirestoreError } from "../utils/firestoreDebug";
+import { logFirestoreError } from "../../firestoreDebug.js";
 
 
 export function ContactForm() {
